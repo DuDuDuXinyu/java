7c0741858329e474b7bbeb50fb190c633108c2eb
杜欣宇 <2331102566@qq.com>
2020-03-16 10:24:53 +0800

    计算器

--- Calculator.java
+++ Calculator.java
@@ -1,34 +1,95 @@
 public class Calculator
 {
+	
 	public static void main (String []args){
-		Calculator.add(10,20);//30
-		Calculator.sub(20,10);//10
-		Calculator.mul(10,20);//200
-		Calculator.div(20,10);//2
-		Calculator.div(10,0);//无法计算
+		
+		int choose=0;
+		java.util.Scanner scanner=new java.util.Scanner(System.in);
+		while(choose<5)
+		{
+			menu();
+			choose=scanner.nextInt();
+			if(choose==5)
+			{
+				break;
+			}
+			switch(choose)
+			{
+				case 1:add();break;
+				case 2:sub();break;
+				case 3:mul();break;
+				case 4:div();break;
+				default:break;
+			}
+			
+		}
 	}
-	public static void add(int a,int b){
+
+	public static void menu()
+	{
+		System.out.println("--------------------------");
+		System.out.println("------欢迎使用计算器------");
+		System.out.println("--------------------------");
+		System.out.println();
+		System.out.println("   请选择你要做的计算 ：  ");
+		System.out.println();
+		System.out.println("   1、加法      2、减法   ");
+		System.out.println();
+		System.out.println("   3、乘法      4、除法   ");
+		System.out.println();
+		System.out.println("----------5、退出---------");
+	}
+
+	public static void add(){
+		
+		int a,b;
+		java.util.Scanner scanner=new java.util.Scanner(System.in);
+		System.out.println("输入第一个加数 ： ");
+		a=scanner.nextInt();
+		System.out.println("输入第二个加数 ： ");
+		b=scanner.nextInt();
 
 		//加法运算
 
 		int c=a+b;
 		System.out.println(a+" + "+b+" = "+c);
 	}
-	public static void sub(int a,int b){
+	public static void sub(){
+		
+		int a,b;
+		java.util.Scanner scanner=new java.util.Scanner(System.in);
+		System.out.println("输入被减数 ： ");
+		a=scanner.nextInt();
+		System.out.println("输入减数 ： ");
+		b=scanner.nextInt();
 
 		//减法运算
 
 		int c=a-b;
 		System.out.println(a+" - "+b+" = "+c);
 	}
-	public static void mul(int a,int b){
+	public static void mul(){
+
+		int a,b;
+		java.util.Scanner scanner=new java.util.Scanner(System.in);
+		System.out.println("输入第一个乘数 ： ");
+		a=scanner.nextInt();
+		System.out.println("输入第二个乘数 ： ");
+		b=scanner.nextInt();
 
 		//乘法运算
 
 		int c=a*b;
 		System.out.println(a+" * "+b+" = "+c);
 	}
-	public static void div(int a,int b){
+	public static void div(){
+
+		int a,b;
+		java.util.Scanner scanner=new java.util.Scanner(System.in);
+		System.out.println("输入被除数 ： ");
+		a=scanner.nextInt();
+		System.out.println("输入除数 ： ");
+		b=scanner.nextInt();
 
 		//除法运算
 
