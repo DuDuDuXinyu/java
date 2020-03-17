e7919f9316dcf890ee04ccdef353beae4753729f
杜欣宇 <2331102566@qq.com>
2020-03-17 23:48:32 +0800

    输入年龄判断你的属于什么年龄段

---
+++ JudgeAge.java
@@ -0,0 +1,22 @@
+public class JudgeAge
+{
+	public static void main(String []args){
+		java.util.Scanner s=new java.util.Scanner(System.in);
+		int age=s.nextInt();
+		if(age<=12){
+			System.out.println("您是一个儿童！");
+		}
+		else if(age<18){
+			System.out.println("您是一个青少年！");
+		}
+		else if(age<30){
+			System.out.println("您是一个青年！");
+		}
+		else if(age<50){
+			System.out.println("您是一个壮年！");
+		}
+		else{
+			System.out.println("您是一个老年人！");
+		}
+	}
+}
