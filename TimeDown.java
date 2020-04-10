59a01110ce59249097f645cbca10ef8b53ddaf6d
杜欣宇 <2331102566@qq.com>
2020-04-10 16:11:54 +0800

    倒计时（有个线程中断操作）

---
+++ TimeDown.java
@@ -0,0 +1,27 @@
+public class TimeDown
+{
+	public static void main(String []args){
+		System.out.print("请输入倒计时的时间（以秒为单位）： ");
+		java.util.Scanner s=new java.util.Scanner(System.in);
+		int num=s.nextInt();
+		int a=num;
+		while(num!=0){
+			System.out.print(num);
+			num--;
+			System.out.print("\r");
+
+			//线程终端也就是睡眠
+			//固定格式
+
+			try {
+                 Thread.sleep(1000);
+ 
+             } catch (InterruptedException e) {
+                 e.printStackTrace();
+             }
+
+
+		}
+		System.out.print(a+"s时间已经到了！");
+	}
+}
